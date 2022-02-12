17c66b5c1e2e2ace9e073c17555309808dd46ce9
taraz14 <bonaventura.pradhana@students.amikom.ac.id>
2022-02-13 01:35:42 +0900

    partials layout

---
+++ src/layout/partials/Footer.part.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Footer = () => {
+  return (
+    <div className="text-center copy-right">
+      &copy; 2022 Dimas P. Pradhana, learn MERN React
+    </div>
+  );
+};
